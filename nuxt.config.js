c9a1f436206e36dab41288206337d29537753e35
Pimuk <98146649+pimukchall@users.noreply.github.com>
2024-03-14 16:58:01 +0700

    auth 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'http://localhost:3001',
-    credentials: false,
+    credentials: true,
   },
   auth: {
     redirect: {
